6312bfa5a35294bc170444d1680a7e3ca1922628
glasses4days <cooper@Coopers-MacBook-Air.local>
2016-04-24 12:51:15 -0700

    added base.html

--- application.py
+++ application.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import jinja2
 
 app = Flask(__name__)
 
+app.jinja_env.undefined = jinja2.StrictUndefined
 
 @app.route("/")
 def index_page():
@@ -14,6 +16,26 @@ def index_page():
     #
     # return render_template("index.html")
 
+@app.route("/application-form")
+def application():
+    """Complete job application form here."""
+
+    return render_template("application-form.html")
+
+@app.route("/application", methods=["POST"])
+def application_submit():
+    """Returns info from application form"""
+
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    salary = request.form.get("salary")
+    job = request.form.get("job")
+
+    return render_template("/application-response.html",
+                            firstname=firstname,
+                            lastname=lastname,
+                            salary=salary,
+                            job=job)
 
 if __name__ == "__main__":
     app.run(debug=True)
